a231364fe77d554551c4277429c8a5b2cd2f8d7b
G-Papad <geopapadoulis@gmail.com>
2022-02-26 18:47:16 +0200

    models.py Database

--- expressisverbis/ExpressisVerbisApp/admin.py
+++ expressisverbis/ExpressisVerbisApp/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import *
+
+
 
 # Register your models here.
+
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Sponsor)
+class SponsorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Update)
+class UpdateAdmin(admin.ModelAdmin):
+    pass
--- expressisverbis/ExpressisVerbisApp/models.py
+++ expressisverbis/ExpressisVerbisApp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Issue(models.Model):
+    issueNumber = models.IntegerField(unique=True)
+    publicationDate = models.DateTimeField()
+    issueFile = models.FileField(upload_to='issue/')
+
+class Sponsor(models.Model):
+    name = models.CharField(max_length=50)
+    logo = models.ImageField(upload_to='sponsors/') 
+    link = models.URLField()
+    info = models.TextField()
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    rank = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='team/')
+    description = models.TextField()
+
+class Update(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200)
+    publicationDate = models.DateTimeField()
+    photo = models.ImageField(upload_to='updates/')
+    text = models.TextField()
+
