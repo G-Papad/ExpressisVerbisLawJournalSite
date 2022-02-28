aa528289d701d04987fb94ab759c000ad7a92825
Kokkinakis <el18115@mail.ntua.gr>
2022-02-28 15:11:35 +0200

    urls and views functions

---
+++ expressisverbis/ExpressisVerbisApp/urls.py
@@ -0,0 +1,14 @@
+from . import views
+from django.urls import path, include
+from . import views
+# URLConfiguration
+
+urlpatterns = [
+    path('main', views.mainpage, name='mainpage'),
+    path('issues', views.issues, name='issues'),
+    path('members', views.members, name='members'),
+    path('contact', views.contact, name='contact'),
+    path('team', views.team, name='team'),
+    path('sponsors', views.sponsors, name='sponsors'),
+    path('announcements', views.announcements, name='announcements'),
+]
--- expressisverbis/ExpressisVerbisApp/views.py
+++ expressisverbis/ExpressisVerbisApp/views.py
@@ -1,4 +1,35 @@
 from django.shortcuts import render
+import stmplib
 
 # Create your views here.
 
+
+def mainpage(request):
+    return render(request, 'mainpage.html')
+
+
+def issues(request):
+    return render(request, 'issues.html')
+
+
+def members(request):
+    return render(request, 'members.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = request.POST
+        print(form)
+    return render(request, 'contact.html')
+
+
+def team(request):
+    return render(request, 'team.html')
+
+
+def sponsors(request):
+    return render(request, 'sponsors.html')
+
+
+def announcements(request):
+    return render(request, 'announcements.html')
