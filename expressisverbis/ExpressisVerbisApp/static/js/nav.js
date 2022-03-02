a11f223eafa95d5d56ea897b1b07d5880e62b87e
G-Papad <geopapadoulis@gmail.com>
2022-03-02 23:26:34 +0200

    navbar color currentLink

--- expressisverbis/ExpressisVerbisApp/static/js/nav.js
+++ expressisverbis/ExpressisVerbisApp/static/js/nav.js
@@ -1,5 +1,15 @@
 let burger = document.getElementsByClassName('toggle-button')[0];
 let links = document.getElementsByClassName('navbar-links')[0];
+let links_buttons = Array.from(links.children[0].children);
+
 burger.addEventListener('click', () => {
     links.classList.toggle('active');
+});
+
+//function in order to color current link in navbar
+links_buttons.forEach(element => {
+    let current = element.childNodes[0];
+    if(current.href==window.location.href){
+        current.classList.add('currentLink');
+    }
 });
