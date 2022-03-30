bd35677f97503cea877ab35850e0af7f9b1550bf
G-Papad <geopapadoulis@gmail.com>
2022-03-30 19:37:13 +0300

    no commit

--- expressisverbis/ExpressisVerbisApp/static/js/nav.js
+++ expressisverbis/ExpressisVerbisApp/static/js/nav.js
@@ -1,14 +1,17 @@
 let burger = document.getElementsByClassName('toggle-button')[0];
 let links = document.getElementsByClassName('navbar-links')[0];
+console.log(links.children[0]);
 let links_buttons = Array.from(links.children[0].children);
 
 burger.addEventListener('click', () => {
     links.classList.toggle('active');
 });
-
+console.log(links_buttons)
 //function in order to color current link in navbar
 links_buttons.forEach(element => {
+    console.log(element)
     let current = element.childNodes[0];
+    // console.log(current)
     if(current.href==window.location.href){
         current.classList.add('currentLink');
     }
